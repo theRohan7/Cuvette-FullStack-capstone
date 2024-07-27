4eca931b7e8ccff237d7af9985086b6b66e3b3c5
Rohan sahu <therohansahu7@gmail.com>
2024-07-28 00:09:42 +0530

    added JobSchema  model file

---
+++ src/models/job.model.js
@@ -0,0 +1,60 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const jobSchema = new Schema(
+    {
+        companyName: {
+            type: String,
+            required: true
+        },
+        logoURL: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String,
+            required: true
+        },
+        salary: {
+            type: Number,
+            required: true
+        },
+        jobType: {
+            type: String,
+            enum: ["Full-time", "Part-time", "Contract", "Freelance"],
+            default: "Full-time"
+        },
+        remote: {
+            type: Boolean,
+            required: true
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        about: {
+            type: String
+        },
+        skills: {
+           type: Array,
+           required: true  
+        },
+        information: {
+            type: String
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+
+    }
+)
+
+export const Job = mongoose.model("Job", jobSchema);
+
+
+
