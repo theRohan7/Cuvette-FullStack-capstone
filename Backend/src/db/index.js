ba20bf76695cd122b26d3202743e6d6c7452a38d
Rohan sahu <therohansahu7@gmail.com>
2024-07-30 12:28:10 +0530

    changed older directory

---
+++ Backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const DB_NAME = "Cuvette";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB Connected successfully!! DB HOST: ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log("MONGO DB Connection failed.", error);
+        process.exit(1)
+        
+    }
+}
+
+export default connectDB;
